0f7d52702c938c9f9821a535cfcc7ed0d5ccde27
Bilopp <philipp@thiel.team>
2025-04-14 17:08:05 +0200

    restored part of old fish config

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -1,11 +1,15 @@
 { config, pkgs, ... }: {
   # shell
+  home.packages = with pkgs; [ lsd zoxide ];
   programs.starship.enable = true;
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
+      alias old_ls="ls"
+      alias ls="lsd"
       set fish_greeting
       starship init fish | source
+      zoxide init fish --cmd cd | source
     '';
   };
 }
